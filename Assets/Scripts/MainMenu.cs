947e6cba9282dbe8769174ab1c0c155f36826c18
Evan Edwards <ejedwards@mun.ca>
2021-04-15 15:00:48 -0700

    Added main menu scene.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Boolean isStart;
+
+    public Boolean isQuit;
+
+    private void OnMouseUp()
+    {
+        if (isStart)
+        {
+            SceneManager.LoadScene("Defend");
+        }
+
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
